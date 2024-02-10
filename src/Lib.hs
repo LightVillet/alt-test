f967095c62a71237b01fd85618f43025639716d8
LightVillet <dev@null>
2024-02-10 13:49:12 +0300

    add arch grouping

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,47 +3,55 @@ module Lib (compareBranches) where
 -- TODO: error handling
 
 import qualified    Data.Map.Strict as Map
-import              Control.Applicative (liftA3)
-import              EVRComparison (compareEVR)
-import              Net (getBranchInfo, PackageInfo(..), BranchInfo(..))
+import qualified    Data.Map.Merge.Strict as Map.Merge
+import qualified    EVRComparison as EVR
+import qualified    Net (getBranchInfo, PackageInfo(..), BranchInfo(..))
 
 
 type Branch = String
 
 type PackageName = String
+type PackageArch = String
 type PackageEpoch = Integer
 type PackageVersion = String
 type PackageRelease = String
 -- EVR is for epoch-version-release triplet
 type PackageEVR = (PackageEpoch, PackageVersion, PackageRelease)
-type PackageTuple = (PackageName, PackageEVR)
-type PackagesMap = Map.Map PackageName PackageEVR
 
-
-data BranchDiff = BranchDiff {
-    extraPackages   :: [PackageTuple],
-    missingPackages :: [PackageTuple],
+data ArchDiff = ArchDiff {
+    extraPackages   :: [(PackageName, PackageEVR)],
+    missingPackages :: [(PackageName, PackageEVR)],
     -- Newer EVR and older
     newerPackages   :: [(PackageName, (PackageEVR, PackageEVR))]
 } deriving (Show)
 
-getPackagePair :: PackageInfo -> PackageTuple
-getPackagePair p = (name p, (epoch p, version p, release p))
 
-branchInfoToMap :: BranchInfo -> PackagesMap
-branchInfoToMap = Map.fromList . fmap getPackagePair . packages
+packageInfoToPair :: Net.PackageInfo -> (PackageName, PackageEVR)
+packageInfoToPair p = (Net.name p, (Net.epoch p, Net.version p, Net.release p))
 
-mapADiffToList :: (Applicative f, Ord k) => f (Map.Map k a) -> f (Map.Map k b) -> f [(k, a)]
-mapADiffToList m1 m2 = Map.toList <$> liftA2 Map.difference m1 m2
+branchInfoToMap :: Net.BranchInfo -> Map.Map PackageArch (Map.Map PackageName PackageEVR)
+branchInfoToMap bi = Map.map Map.fromList archMap
+    where archMap = Map.fromListWith (++) $ map (\p -> (Net.arch p, [packageInfoToPair p])) $ Net.packages bi
 
-compareBranches :: Branch -> Branch -> IO (Maybe BranchDiff)
+
+compareArches :: Map.Map PackageName PackageEVR -> Map.Map PackageName PackageEVR -> ArchDiff
+compareArches m1 m2 = ArchDiff { extraPackages=extra, missingPackages=missing, newerPackages=newer }
+    where   extra   = Map.toList $ Map.difference m1 m2
+            missing = Map.toList $ Map.difference m2 m1
+            inter   = Map.toList $ Map.intersectionWith (,) m1 m2
+            newer   = (filter $ (==GT) . uncurry EVR.compareEVR . snd) inter
+
+newArchToArchDiff :: Map.Map PackageName PackageEVR -> ArchDiff
+newArchToArchDiff m = ArchDiff { extraPackages=Map.toList m, missingPackages=[], newerPackages=[]}
+
+compareBranches :: Branch -> Branch -> IO (Maybe (Map.Map PackageArch ArchDiff))
 compareBranches fstBranch sndBranch = do
-    bInfoFst <- getBranchInfo fstBranch
-    bInfoSnd <- getBranchInfo sndBranch
-    let packagesFst = fmap branchInfoToMap bInfoFst
-    let packagesSnd = fmap branchInfoToMap bInfoSnd
-    let extra = mapADiffToList packagesFst packagesSnd
-    let missing = mapADiffToList packagesSnd packagesFst
-    let inter = Map.toList <$> liftA2 (Map.intersectionWith (,)) packagesFst packagesSnd
-    let newer = fmap (filter $ (==GT) . uncurry compareEVR . snd) inter
-    return $ liftA3 BranchDiff extra missing newer
+    bInfoFst <- Net.getBranchInfo fstBranch
+    bInfoSnd <- Net.getBranchInfo sndBranch
+    let m1 = fmap branchInfoToMap bInfoFst
+    let m2 = fmap branchInfoToMap bInfoSnd
+    let extraArch = Map.Merge.mapMissing $ const newArchToArchDiff
+    let missingArch = Map.Merge.mapMissing $ const newArchToArchDiff
+    let sameArch = Map.Merge.zipWithMatched $ const compareArches
+    let diff = liftA2 (Map.Merge.merge extraArch missingArch sameArch) m1 m2
+    return diff
